9a649e2aca5e4977ae6c60168ef7ed3c7668e8e3
danielkamweru <daniel.kamweru@student.moringaschool.com>
2025-11-20 20:55:38 +0300

    complete lab

--- lib/versions.py
+++ lib/versions.py
@@ -3,9 +3,7 @@ import requests
 import pytest
 
 def python_version():
-    from collections import namedtuple
-    VersionInfo = namedtuple('VersionInfo', ['major', 'minor', 'micro', 'releaselevel', 'serial'])
-    return VersionInfo(3, 8, 13, 'final', 0)
+    return sys.version_info
 
 def requests_version():
     return requests.__version__
